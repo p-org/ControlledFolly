51ec9241a4e08b1e44009e9f65a6a1dc57491d04
Arun T <t-arut@microsoft.com>
2019-11-13 11:09:35 +0000

    Deleted Nekara

--- Nekara/Core/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nekara.Core
-{
-    public static class Constants
-    {
-        public const int SessionTimeoutMs = 5000;
-        public const int SessionMaxDecisions = 1000;
-    }
-}
